80e063eedc27a3bfc9c8963d6468838773ec0030
aydin <aydin@AYDIN>
2026-04-08 21:43:30 +0300

    2.gun tamamlandi

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using RestAprilEducationRepository.Application.Products;
+using RestAprilEducationRepository.Application.Products.GetList;
+using RestAprilEducationRepository.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +9,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+
+// DI Container Framework ( Library ) -
+
+// DI + IoC => DI - Dependency Injection Pattern
+
+// Singleton
+// Transient
+// Scoped
+
+// Transient => her seferinde yeni bir instance oluşturulur. ( stateless )
+// Singleton => uygulama boyunca tek bir instance oluşturulur. ( stateful )
+// Scoped => her istek için yeni bir instance oluşturulur. ( stateful )
+
+// Transient >> Scoped > Singleton
+
+//builder.Services.AddSingleton<CalculateService>();
+builder.Services.AddSingleton<ICalculateService, CalculateService>();
+builder.Services.AddScoped<IProductsApplication, ProductsApplication>();
+builder.Services.AddScoped<IProductRepository, ProductRepositoryWithInMemory>();
+
 
 var app = builder.Build();
 
