36d78da862f2e5be1089dd37347651d9df33228b
aydin <aydin@AYDIN>
2026-04-22 22:17:59 +0300

    Add local NuGet package source to project config
    Added C:\UdemyTelcoCrmMicroservices\Packages as a NuGet package source in project configuration files. Updated dgSpecHash in project.nuget.cache to reflect this change. This allows NuGet to resolve packages from the specified local directory in addition to existing sources.

--- RestAprilEducationRepository.Persistence/obj/Debug/net10.0/RestAprilEducationRepository.Persistence.AssemblyInfo.cs
+++ RestAprilEducationRepository.Persistence/obj/Debug/net10.0/RestAprilEducationRepository.Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestAprilEducationRepository.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18e5b7d5e280c283cda6328f344ae6cc4ba14d1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb4eaacab7486e9a0a7a337f8488a0e8439f1f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestAprilEducationRepository.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestAprilEducationRepository.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
